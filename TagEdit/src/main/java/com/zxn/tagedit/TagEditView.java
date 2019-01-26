d64a480c38f0c858085d0b26455b73660b4bc94b
zxn <zhangxiaoning@avicare.cn>
2019-01-26 23:22:18 +0800

    Initial commit

---
+++ TagEdit/src/main/java/com/zxn/tagedit/TagEditView.java
@@ -0,0 +1,106 @@
+package com.zxn.tagedit;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * 一个带标记标记的输入框.
+ * Created by zxn on 2019/1/26.
+ */
+public class TagEditView extends LinearLayout {
+
+
+    private TextView tvTag;
+    private EditText etInput;
+    private String TAG = "TagEditView";
+
+    public TagEditView(Context context) {
+        this(context, null);
+    }
+
+    public TagEditView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TagEditView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        onInit(attrs, defStyleAttr);
+    }
+
+    private void onInit(AttributeSet attrs, int defStyleAttr) {
+        onInitView();
+
+        final TypedArray typedArray
+                = getContext()
+                .obtainStyledAttributes(attrs, R.styleable.TagEditView, defStyleAttr, 0);
+        CharSequence mTagName = typedArray.getText(R.styleable.TagEditView_tagName);
+        CharSequence mHintText = typedArray.getText(R.styleable.TagEditView_hintText);
+
+        int mTagNameColor = typedArray.getColor(R.styleable.TagEditView_tagNameColor, Color.parseColor("#a8b1b2"));
+        int mHintTextColor = typedArray.getColor(R.styleable.TagEditView_hintTextColor, Color.parseColor("#a7acab"));
+
+        //此方法读取的属性值的单位为px
+        int dTagNameSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 15, getResources().getDisplayMetrics());
+        int mTagNameSize = typedArray.getDimensionPixelSize(R.styleable.TagEditView_tagNameSize, dTagNameSize);
+        //float mTagNameSize = typedArray.getDimension(R.styleable.TagEditView_tagNameSize, 15);
+
+        int dHintTextSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 13, getResources().getDisplayMetrics());
+        int mHintTextSize = typedArray.getDimensionPixelSize(R.styleable.TagEditView_hintTextSize, dHintTextSize);
+        Log.i(TAG, "onInit: -->" + mTagNameSize);
+        //onInit: -->52--->15
+        //onInit: -->51.749996
+
+
+        Log.i(TAG, "onInit: -->" + mHintTextSize);
+        //onInit: -->13
+        tvTag.setText(mTagName);
+        //tvTag.setTextSize(TypedValue.COMPLEX_UNIT_SP, mTagNameSize);
+        tvTag.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTagNameSize);
+        tvTag.setTextColor(mTagNameColor);
+
+        etInput.setHint(mHintText);
+        etInput.setHintTextColor(mHintTextColor);
+        etInput.setTextSize(TypedValue.COMPLEX_UNIT_PX, mHintTextSize);
+        typedArray.recycle();
+    }
+
+    private void onInitView() {
+        LayoutInflater.from(getContext())
+                .inflate(R.layout.layout_tag_edit_view, this, true);
+        setOrientation(HORIZONTAL);
+        tvTag = findViewById(R.id.tv_tag);
+        etInput = findViewById(R.id.et_input);
+    }
+
+/*    public static float applyDimension(int unit, float value,
+                                       DisplayMetrics metrics)
+    {
+        switch (unit) {
+            case COMPLEX_UNIT_PX:
+                return value;
+            case COMPLEX_UNIT_DIP:
+                return value * metrics.density;
+            case COMPLEX_UNIT_SP:
+                return value * metrics.scaledDensity;
+            case COMPLEX_UNIT_PT:
+                return value * metrics.xdpi * (1.0f/72);
+            case COMPLEX_UNIT_IN:
+                return value * metrics.xdpi;
+            case COMPLEX_UNIT_MM:
+                return value * metrics.xdpi * (1.0f/25.4f);
+        }
+        return 0;
+    }*/
+
+
+}
---
+++ app/src/main/java/com/zxn/tagedittextdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zxn.tagedittextdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by zxn on 2019-1-26 21:57:35.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
